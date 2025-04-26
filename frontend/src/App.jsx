d55644dfc35eb1d76e3007282485216def109aaa
kiereiaayman <kiereiaayman@gmail.com>
2025-04-26 16:46:54 +0300

    marketplace added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout/Layout'; // Adjust the path
+<<<<<<< HEAD
 import Marketplace from './pages/Home/Marketplace'; // Adjust the path
 import SearchResults from './pages/Home/SearchResult'; // Adjust the path
 
@@ -18,6 +19,32 @@ function App() {
     </Router>
     
   );
+=======
+import Login from './components/Login/Login';
+
+
+
+//to test the login page 
+function App(){
+  return(
+    <div>
+      <Login/>
+    </div>
+  )
+>>>>>>> front-end
 }
+
+
+// function App() {
+//   const user="John Doe"; // Replace with the actual name you want to pass
+//   return (
+//     <Router>
+//       <Routes>
+//         <Route path="/" element={<Layout name={user}/>} />
+//       </Routes>
+//     </Router>
+    
+//   );
+// }
 
 export default App;
