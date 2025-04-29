a21d198dcb04f18906d2e9cfa544ca7e5093badb
michealshohda <michealshohda2015@gmail.com>
2025-04-29 21:38:29 +0300

    adding profile page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,36 +3,9 @@ import Layout from './components/Layout/Layout'; // Adjust the path
 
 import Marketplace from './pages/Home/Marketplace'; // Adjust the path
 import SearchResults from './pages/Home/SearchResult'; // Adjust the path
-
-function App() {
-  const user="John Doe"; // Replace with the actual name you want to pass
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Layout name={user}/>} >
-          <Route path="/" element={<Marketplace />} />
-          <Route path=":category" element={<Marketplace />} />
-
-          <Route path="/search" element={<SearchResults />} />
-        </Route>
-      </Routes>
-    </Router>
-    
-  );
-=======
 import Login from './components/Login/Login';
+import Profile from './pages/Profile/Profile'; 
 
-
-
-//to test the login page 
-function App(){
-  return(
-    <div>
-      <Login/>
-    </div>
-  )
->>>>>>> front-end
-}
 
 
 // function App() {
@@ -40,11 +13,28 @@ function App(){
 //   return (
 //     <Router>
 //       <Routes>
-//         <Route path="/" element={<Layout name={user}/>} />
+//         <Route path="/" element={<Layout name={user}/>} >
+//           <Route path="/" element={<Marketplace />} />
+//           <Route path=":category" element={<Marketplace />} />
+
+//           <Route path="/search" element={<SearchResults />} />
+//         </Route>
 //       </Routes>
 //     </Router>
     
 //   );
+
 // }
 
-export default App;
+
+//to test the login page 
+export default function App(){
+  return(
+    <div>
+      <Profile />
+    </div>
+  )
+
+}
+
+ 
